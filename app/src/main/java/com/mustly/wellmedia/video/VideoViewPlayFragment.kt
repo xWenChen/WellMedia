8a3b9b235dae1d28688778821add914aad401a7a
wenchen <wenchenwc@qq.com>
2022-06-16 23:50:30 +0800

    feat: VideoView 例子待补充

--- app/src/main/java/com/mustly/wellmedia/video/VideoViewPlayFragment.kt
+++ app/src/main/java/com/mustly/wellmedia/video/VideoViewPlayFragment.kt
@@ -1,8 +1,12 @@
 package com.mustly.wellmedia.video
 
 import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.MediaController
 import com.mustly.wellmedia.R
 import com.mustly.wellmedia.base.BaseFragment
 import com.mustly.wellmedia.base.PageRoute
@@ -11,13 +15,36 @@ import com.mustly.wellmedia.lib.annotation.Route
 
 @Route(PageRoute.VIDEO_VIEW_PLAY)
 class VideoViewPlayFragment : BaseFragment<FragmentVideoViewPlayBinding>(R.layout.fragment_video_view_play) {
+    companion object {
+        const val TAG = "VideoViewPlayFragment"
+    }
 
     override fun initArguments(savedInstanceState: Bundle?) {
 
     }
 
     override fun initView(rootView: View) {
+        binding.videoView.apply {
+            // 播放 res/raw 目录下的文件
+            setVideoURI(Uri.parse("android.resource://${requireActivity().packageName}/${R.raw.}"))
 
+            setMediaController(MediaController(requireActivity()))
+
+            setOnPreparedListener {
+                Log.d(TAG, " >>> Video prepared")
+            }
+            setOnCompletionListener {
+                Log.d(TAG, " >>> Video play completed")
+            }
+
+            setOnErrorListener { mediaPlayer, what, extra ->
+                Log.e(TAG, " >>> Video play error, what=$what, extra=$extra")
+                // 返回 true, 表示错误被处理，不显示 VideoView 自定义的弹框
+                true
+            }
+            // 开始播放
+            start()
+        }
     }
 
     override fun initData(context: Context) {
