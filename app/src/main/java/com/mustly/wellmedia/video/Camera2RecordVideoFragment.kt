77823fa82c6a97687fa6a64e20b3c05ab2ac9acb
wchenzhang <wchenzhang@tencent.com>
2023-04-28 16:24:37 +0800

    feat(实现 Camera2 录制视频功能): TODO 实现 Camera2 录制视频

--- app/src/main/java/com/mustly/wellmedia/video/Camera2RecordVideoFragment.kt
+++ app/src/main/java/com/mustly/wellmedia/video/Camera2RecordVideoFragment.kt
@@ -1,6 +1,8 @@
-package com.mustly.wellmedia.image
+package com.mustly.wellmedia.video
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Context
 import android.graphics.Color
 import android.graphics.ImageFormat
@@ -11,20 +13,23 @@ import android.media.ImageReader
 import android.os.Build
 import android.os.Handler
 import android.os.HandlerThread
+import android.provider.MediaStore
+import android.util.Log
+import android.view.Surface
 import android.view.SurfaceHolder
 import android.view.View
+import android.widget.Toast
 import androidx.core.graphics.drawable.toDrawable
 import androidx.lifecycle.lifecycleScope
+import com.mustly.wellmedia.R
 import com.mustly.wellmedia.base.BaseFragment
 import com.mustly.wellmedia.base.PageRoute
-import com.mustly.wellmedia.databinding.FragmentCamera2TakePhotoBinding
+import com.mustly.wellmedia.databinding.FragmentCamera2RecordBinding
 import com.mustly.wellmedia.lib.annotation.Route
 import com.mustly.wellmedia.lib.commonlib.log.LogUtil
 import com.mustly.wellmedia.lib.commonlib.utils.*
-import kotlinx.coroutines.CancellableContinuation
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.suspendCancellableCoroutine
+import com.mustly.wellmedia.utils.*
+import kotlinx.coroutines.*
 import java.io.File
 import java.io.FileOutputStream
 import java.text.SimpleDateFormat
@@ -34,18 +39,42 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 /**
- * todo 待讲解列表：1. Surface+SurfaceView, ImageReader, Camera2 框架
+ * MediaCodec + Camera2 录制视频
  *
- * Camera2 拍照功能，代码参考自官方示例：https://github.com/android/camera-samples
+ * https://developer.android.com/training/camera2
  *
- * 使用 Camera2 拍照的步骤为：
+ * 相机的图像方向，通常和手机的方向相差 90 度，设备必须旋转 SENSOR_ORIENTATION 度数(可选度数：0、90、180、270)，
+ * 前置摄像头逆时针选项，后置摄像头顺时针旋转：
+ *  https://developer.android.com/static/images/training/camera/camera2/camera-preview/camera_preview_portrait_front_facing.png
  *
- * 1. 检查 <uses-permission android:name="android.permission.CAMERA" /> 权限
+ * 摄像头图像的旋转度数，需要是 SENSOR_ORIENTATION + 设备旋转度数
+ *
+ * Camera2 使用步骤：
+ *  1. 获取 CameraManager
+ *  2. 获取 CameraManager#CameraCharacteristics
+ *  3. 打开相机，获取 CameraDevice 并设置回调
+ *
+ * Camera2 使用第一步 >>> 获取目标 surface，surface 可能来自于不同地方
+ *      1. 如果想把图像直接展示给用户，可以使用 SurfaceView 的 surface
+ *      2. 如果想要读取每一帧图像或者执行逐帧分析，可以使用 ImageReader 的 surface
+ *      3. 如果进行并行处理，可以使用 RenderScript.Allocation 的 surface
+ *      4. 尽管不推荐使用(基于可维护性考虑)，也可以使用 OpenGL Texture 或 TextureView 的 surface
  * */
-@Route(PageRoute.CAMERA2_TAKE_PHOTO)
-class Camera2TakePhotoFragment : BaseFragment<FragmentCamera2TakePhotoBinding>() {
+@Route(PageRoute.CAMERA2_RECORD_VIDEO)
+class Camera2RecordVideoFragment : BaseFragment<FragmentCamera2RecordBinding>() {
     companion object {
-        const val TAG = "Camera2TakePhotoFragment"
+        const val TAG = "Camera2RecordVideoFragment"
+
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+
+        private val REQUIRED_PERMISSIONS = mutableListOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+        ).apply {
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }.toTypedArray()
 
         /**
          * 图片的最大缓存数量
@@ -79,7 +108,7 @@ class Camera2TakePhotoFragment : BaseFragment<FragmentCamera2TakePhotoBinding>()
     // 用于获取拍摄的图片数据
     private var imageReader: ImageReader? = null
 
-    private val animationTask: Runnable by lazy {
+    private val animationTask: java.lang.Runnable by lazy {
         Runnable {
             // 白色动画模拟曝光
             binding.overlay.background = Color.argb(150, 255, 255, 255).toDrawable()
