3a871eceebcc97d3a9c82a493e645876ef1ee354
wchenzhang <wchenzhang@tencent.com>
2025-01-18 15:38:24 +0800

    feat: 实现音频播放图片旋转的效果。

--- app/src/main/java/com/mustly/wellmedia/audio/AudioPlayFragment.kt
+++ app/src/main/java/com/mustly/wellmedia/audio/AudioPlayFragment.kt
@@ -1,7 +1,10 @@
 package com.mustly.wellmedia.audio
 
-import android.content.Context
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.LinearInterpolator
+import com.mustly.wellmedia.R
 import com.mustly.wellmedia.base.*
 import com.mustly.wellmedia.databinding.FragmentAudioPlayBinding
 import com.mustly.wellmedia.lib.annotation.Route
@@ -9,8 +12,22 @@ import com.mustly.wellmedia.lib.annotation.Route
 @Route(PageRoute.AUDIO_PLAY_FRAGMENT)
 class AudioPlayFragment : BaseBindingFragment<FragmentAudioPlayBinding>() {
 
+    var anim: Animation? = null
+
     override fun initView(rootView: View) {
         // TODO 实现播放音乐时，柱状图的特效/图片转圈的特效
+        // 图片转圈的特效
+        anim = AnimationUtils.loadAnimation(requireContext(), R.anim.rotate)
+        anim?.interpolator = LinearInterpolator()
+        binding.ivRotate.animation = anim
+        anim?.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.ivRotate.clearAnimation()
+        anim?.cancel()
+        anim = null
     }
 
     override fun initData(rootView: View) {
